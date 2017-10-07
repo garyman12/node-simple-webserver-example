b7cd8c6842cabf602d728ab7dcb9d1278e149c29
Adam <Adamku19@mybedford.us>
2017-10-07 17:00:08 -0400

    a
    a

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ console.log(buttonState);          // write the value to the console for debuggi
 var i = 0;
 function disp(){
   console.log("The current temp isa:");
-  console.log("Vibration is at:" + this.buttonState); // put in later
+  console.log("Vibration is at:" + buttonState); // put in later
 }
 console.log('Server running at http://'+ localIP +':'+ port +'/');
 
